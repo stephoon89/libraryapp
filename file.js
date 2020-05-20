7412b5653271c73671cdc4fe9147f58b4d9078d7
stephoon <stephankaeschel@gmail.com>
2020-05-20 22:08:59 +0200

    created all basic functions of the app. add, remove and rendering works now. added IDs to objects

--- file.js
+++ file.js
@@ -1,10 +1,11 @@
+let bookcards = document.querySelector(".bookcards");
 
 let myLibrary = [
     {
         title: "Winning Ugly",
         author: "Brad Gilbert",
         pages: 224,
-        read: 1
+        read: 1,
     }
 ];
 
@@ -13,9 +14,7 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() { 
-        return `${title} by ${author}, ${pages} pages`;
-    }
+    // possibly refactor this.info to prototype
 }
 
 function addBookToLibrary () {
@@ -28,3 +27,49 @@ function addBookToLibrary () {
 
     myLibrary.push(new Book(title, author, pages));
 }
+
+let newItem;
+let index;
+
+function render() {
+    // removes all HTML bookcard elements
+    while(bookcards.firstChild) {
+        bookcards.removeChild(bookcards.lastChild);
+    }
+
+    // creates new HTML bookcards
+    for(index = myLibrary.length -1; index >= 0; index--) {
+    // for(index = 0; index < myLibrary.length; index++) { 
+        newItem = document.createElement("div");
+        newItem.style.background = "red";
+        newItem.innerHTML = myLibrary[index].title;
+        bookcards.appendChild(newItem);
+
+        myLibrary[index].id = index;
+
+        //renders remove button
+        let removeBtn = document.createElement("p");
+        removeBtn.innerHTML = `Remove book`;
+        bookcards.appendChild(removeBtn);
+    
+        let bookIndex = myLibrary[index].id;
+
+        // renders remove function
+        removeBtn.addEventListener("click", function () {
+            myLibrary.splice(bookIndex, 1)
+            render();
+        });
+    }
+}
+
+const addBtn = document.querySelector(".addbtn");
+// think of a smarter name for "something"
+addBtn.onclick = something;
+// the stuff that should happen when you click on add
+function something () {
+    addBookToLibrary();
+    render();
+}
+
+    render();
+
