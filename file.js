c5d2468323b21c4d4be43ef2b015bb2dcd63f8e6
stephoon <stephankaeschel@gmail.com>
2020-05-22 22:59:19 +0200

    added local storage to save library upon page close

--- file.js
+++ file.js
@@ -5,16 +5,19 @@ let myLibrary = [
         title: "Winning Ugly",
         author: "Brad Gilbert",
         pages: 224,
-        read: 0
-        
+        read: "Not read",    
     }
 ];
+
+if(localStorage.length >= 1) {
+    getStoredBooks();
+} 
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = 0
+    this.read = "Not read"
     
     // possibly refactor to prototypes for practice?
 }
@@ -28,6 +31,7 @@ function addBookToLibrary () {
     if(title == "" || author == "" || pages <= 0) { return };
 
     myLibrary.push(new Book(title, author, pages));
+    storeBooks();
 }
 
 let newItem;
@@ -35,6 +39,7 @@ let index;
 let bookRead;
 
 function render() {
+
     // removes all HTML bookcard elements
     while(bookcards.firstChild) {
         bookcards.removeChild(bookcards.lastChild);
@@ -43,45 +48,65 @@ function render() {
     // creates new HTML bookcards
     for(index = myLibrary.length -1; index >= 0; index--) {
     // for(index = 0; index < myLibrary.length; index++) { 
-        newItem = document.createElement("div");
+        newBookCard = document.createElement("div");
+        newBookCard.className = "bookcard";
+        bookcards.appendChild(newBookCard);
+
+        newItem = document.createElement("h3");
+        newItem.className = "book-title"
         newItem.style.background = "red";
-        newItem.innerHTML = myLibrary[index].title;
-        bookcards.appendChild(newItem);
+        newItem.innerHTML = `${myLibrary[index].title} by ${myLibrary[index].author}, ${myLibrary[index].pages} pages` ;
+        newBookCard.appendChild(newItem);
 
         myLibrary[index].id = index;
 
-
         //renders remove button
         let removeBtn = document.createElement("p");
+        removeBtn.className = "remove-btn"
         removeBtn.innerHTML = `Remove book`;
-        bookcards.appendChild(removeBtn);
+        newBookCard.appendChild(removeBtn);
     
         let bookIndex = myLibrary[index].id;
 
         // renders remove function
         removeBtn.addEventListener("click", function () {
             myLibrary.splice(bookIndex, 1)
+            storeBooks();
             render();
         });
-
         
         // renders read toggle
         let readToggle = document.createElement("p");
+        readToggle.className = "readToggle";
         readToggle.innerHTML = myLibrary[index].read;
         readToggle.style.background = "orange";
-        bookcards.appendChild(readToggle);
+        newBookCard.appendChild(readToggle);
 
-        // changes object property read to 0 
+        // changes object property read to 0 or 1
         let i = index;
         readToggle.addEventListener("click", function () {
-           if(myLibrary[i].read == 0) {myLibrary[i].read = 1;}
-           else {myLibrary[i].read = 0;}
+           if(myLibrary[i].read == "Not read") {myLibrary[i].read = "Read";}
+           else {myLibrary[i].read = "Not read";}
            readToggle.innerHTML = myLibrary[i].read;
+           storeBooks();
         });
 
     }
 }
 
+// function that saves library array to localStorage every time a new book is created
+function storeBooks () {
+    let myLibrary_serialized = JSON.stringify(myLibrary)
+    localStorage.setItem("storedLibrary", myLibrary_serialized);
+    console.log(localStorage);
+}
+
+// function that retrieved the books from localStorage
+function getStoredBooks () {
+    let myLibrary_deserialized = JSON.parse(localStorage.getItem("storedLibrary"));
+    console.log(myLibrary_deserialized);
+    myLibrary = myLibrary_deserialized;
+}
 
 const addBtn = document.querySelector(".addbtn");
 // think of a smarter name for "something"
